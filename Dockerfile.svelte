d2f9521a010d57f78746c081a2f9d52f00324f8b
Antonis Karamitros <ablackjim228@gmail.com>
2020-11-05 14:17:42 +0100

    add docker-compose

---
+++ Dockerfile.svelte
@@ -0,0 +1,19 @@
+FROM node:current-alpine3.12
+
+WORKDIR /home/docker/front-end
+
+COPY ./front-end/package.json .
+COPY ./front-end/package-lock.json .
+
+RUN npm i
+
+COPY ./front-end/rollup.config.js ./
+COPY ./front-end/scripts ./scripts
+COPY ./front-end/public ./public
+COPY ./front-end/src ./src
+
+RUN node --version
+
+ENV HOST=0.0.0.0
+
+CMD npm run dev
